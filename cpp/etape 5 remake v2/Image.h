ac57a6980bac78528a9944355671864f1da10522
Eric Truong <eric.truong@student.hepl.be>
2023-12-04 19:21:10 +0100

    :memo: Creation readme.md

---
+++ cpp/etape 5 remake v2/Image.h
@@ -0,0 +1,42 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+#include "Dimension.h"
+class Image 
+{
+	protected:
+		int id;
+		char*nom;
+		Dimension d;
+
+	public:
+		//variable //
+		static const int L_MAX=500;
+        static const int H_MAX=500;
+      	//constructeur//////
+      	Image();
+     	Image(int i,const char *n);
+     	Image(int i,const char *n,const Dimension &d);
+        Image(const Image &image);
+
+      ////setter//////////
+
+      void setId(int val);
+      void setNom(const char *n);
+      void setDimension(const Dimension &d);
+
+
+       ////getter//////////
+
+      int getId()const;
+      const char *getNom() const;
+      Dimension getDimension()const;
+
+
+      virtual ~Image();//Destructeur virtual mais je peux pas faire sur le constructeur
+      virtual void Affiche() const =0;
+      virtual void Dessine()const =0;
+      virtual void exportToFile(const char* fichier, const char* format) =0;
+
+
+};
+#endif
